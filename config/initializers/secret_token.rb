b58d7c4a3908c278fafc9a7773c25cb8c12188cb
Mary Bennett <yuanwei2@hotmail.com>
2012-08-24 12:09:03 -0400

    Initital commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BlacklightdemoApp::Application.config.secret_token = '7376e109cac73fb8b9f30f365b54d96e0fbfa9638545b750dedd432e8a54c4af1a8aab489e70963196895025e142baa6296d62ade11e14636be3a8b3fe967615'
